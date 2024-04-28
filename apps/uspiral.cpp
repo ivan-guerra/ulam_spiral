81ed40e5f9f7efb34b961f54b12f50ab05af5597
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-04-27 21:15:32 -0500

    Add program arg processing.

--- apps/uspiral.cpp
+++ apps/uspiral.cpp
@@ -1,22 +1,44 @@
-#include <iomanip>
+#include <getopt.h>
+
+#include <cstdlib>
 #include <iostream>
 #include <string>
 
 #include "spiral/spiral.h"
 
+static void PrintUsage() {
+  std::cout << "usage: uspiral [OPTION]..." << std::endl;
+  std::cout << "visualize the Ulam spiral" << std::endl;
+  std::cout << "\t-d, --dimension DIM\n\t\tmatrix dimension" << std::endl;
+  std::cout << "\t-h, --help\n\t\tprint this help page" << std::endl;
+}
+
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    return 1;
-  }
+  std::vector<struct option> longopts{
+      {"dimension", required_argument, 0, 'd'},
+      {"help", no_argument, 0, 'h'},
+      {0, 0, 0, 0},
+  };
 
-  ulam::SquareLattice spiral = ulam::GenerateUlamSpiral(std::stoull(argv[1]));
-
-  for (const ulam::RowVect& row : spiral) {
-    for (const int& val : row) {
-      std::cout << std::setw(2) << val << ' ';
+  int opt = 0;
+  int long_index = 0;
+  std::size_t dim = 0;
+  while (-1 !=
+         (opt = ::getopt_long(argc, argv, "d:h", &longopts[0], &long_index))) {
+    switch (opt) {
+      case 'd':
+        dim = std::stoull(optarg);
+        break;
+      case 'h':
+        PrintUsage();
+        std::exit(EXIT_SUCCESS);
+      case '?':
+        std::exit(EXIT_FAILURE);
     }
-    std::cout << std::endl;
   }
 
-  return 0;
+  ulam::SquareLattice ulam_mat = ulam::GenerateUlamSpiral(dim);
+  /* TODO: Add visualization code. */
+
+  std::exit(EXIT_SUCCESS);
 }
