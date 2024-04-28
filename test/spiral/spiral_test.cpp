a05968a71f929ecee99153f3fcb57150f9cb30fe
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-04-28 10:49:59 -0500

    Add GenerateUlamSpiral() unit tests.

--- test/spiral/spiral_test.cpp
+++ test/spiral/spiral_test.cpp
@@ -2,4 +2,47 @@
 
 #include <gtest/gtest.h>
 
-TEST(SpiralTest, HelloWorld) {}
+#include <fstream>
+#include <optional>
+#include <sstream>
+#include <string>
+
+static const char* kUlamSpiral201Path = "data/uspiral_201.txt";
+
+[[nodiscard]] static ulam::SquareLattice LoadLattice(
+    const std::string& filepath) {
+  ulam::SquareLattice lattice;
+
+  std::ifstream fhandle(filepath);
+  std::string line;
+  while (std::getline(fhandle, line)) {
+    ulam::RowVect row;
+    std::stringstream ss(line);
+    std::string value;
+    while (ss >> value) {
+      row.push_back(std::stoi(value));
+    }
+    lattice.push_back(row);
+  }
+  return lattice;
+}
+
+TEST(SpiralTest, GenUlamSpiralWithZeroDimReturnsNullopt) {
+  std::optional<ulam::SquareLattice> lattice = ulam::GenerateUlamSpiral(0);
+
+  ASSERT_FALSE(lattice);
+}
+
+TEST(SpiralTest, GenUlamSpiralWithNegativeDimReturnsNullopt) {
+  std::optional<ulam::SquareLattice> lattice = ulam::GenerateUlamSpiral(-1);
+
+  ASSERT_FALSE(lattice);
+}
+
+TEST(SpiralTest, GenUlamSpiralWithValidDimReturnsCorrectUlamSpiralLattice) {
+  ulam::SquareLattice expected = LoadLattice(kUlamSpiral201Path);
+  std::optional<ulam::SquareLattice> actual = ulam::GenerateUlamSpiral(201);
+
+  ASSERT_TRUE(actual);
+  ASSERT_EQ(expected, *actual);
+}
